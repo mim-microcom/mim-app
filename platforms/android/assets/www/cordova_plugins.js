39585cd5df7d1d2853372d35083fd552ca1b7c9a
Vincent Coll <vincentgagnnoncoll@gmail.com>
2015-11-03 10:04:05 -0500

    Initial commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,32 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-inappbrowser": "1.0.1",
+    "cordova-plugin-splashscreen": "2.1.0"
+}
+// BOTTOM OF METADATA
+});
